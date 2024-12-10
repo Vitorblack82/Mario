6697bac4f145c4054e172e958f49d05dc2dc18d5
Vitorblack82 <apvitor@gmail.com>
2024-12-09 23:17:10 -0300

    alteração js inicio

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 
+const form = document.querySelector(".formulario-orçamento")
+const mascara = document.querySelector(".mascara-formulario")
+
+function CliqueiNoBotao() {
+
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+
+}
+
+function cliqueiNaMascara() {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0%)"
+}
