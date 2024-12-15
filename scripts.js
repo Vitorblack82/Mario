6ce00aad64ce1a2182ebfbd7af057240c88aaa5d
Vitorblack82 <apvitor@gmail.com>
2024-12-15 13:42:25 -0300

    inclusão de som moeda

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 
 const form = document.querySelector(".formulario-orçamento")
 const mascara = document.querySelector(".mascara-formulario")
-
+const audio =new Audio();
+audio.src ="./audio/coin-257878.mp3"
 
 function CliqueiNoBotao() {
 
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
+
+    
 
 }
 
